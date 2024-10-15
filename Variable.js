469f9dcd7d1b3e09533a115f62c0c6503439441b
AlbinPalma <albin.palma1@gmail.com>
2024-10-14 20:43:40 -0600

    cargar todo lo visto en el precurso

---
+++ Variable.js
@@ -0,0 +1,22 @@
+//string
+var bootcamp = "henry";
+
+//Numeros
+var numerosEnteros = 17;
+var numerosNegativos = -4;
+var numerosDecimales = 11.75;
+
+//Booleanos
+
+var estoyEntendiendo = true;
+
+var estoyAburrido = false;
+
+//undefined
+var cajon1;
+
+console.log(cajon1);
+
+//Null
+
+var sinNada = null;
---
+++ controlDeFlujos.js
@@ -0,0 +1,11 @@
+function viajar(destino){
+    if(destino == "Brazil"){
+        console.log("Gire a la IZQUIERDA")
+    } else if (destino == "Argentina"){
+        console.log("Gire a la DERECHA")
+    }else {
+        console.log("Nos PERDIMOS")
+    }
+}
+
+viajar('Brazil');
